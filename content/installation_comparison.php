8bdc9f5c24056ac69d70dd404d759d9ca1639b0b
Nik Estep <nik.estep@gmail.com>
2014-02-09 14:13:36 -0500

    Re-working installation comparison page for bs3

--- content/installation_comparison.php
+++ content/installation_comparison.php
@@ -1,6 +1,8 @@
-<h3>Comparison of Solar Production</h3>
+<div class="page-header">
+    <h2>Comparison of Solar Production</h2>
+</div>
 
 <p>
-	At this time, metering is not functioning properly so we are unable to
-	display a comparison of solar production.
+    At this time, metering is not functioning properly so we are unable to
+    display a comparison of solar production.
 </p>
