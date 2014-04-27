70b94e9557993b5bcca4e6082418cb95d4a35a03
Nik Estep <nik.estep@gmail.com>
2014-04-27 15:20:44 -0400

    Returning pie charts to home page

--- public_html/statics/script.js
+++ public_html/statics/script.js
@@ -301,21 +301,6 @@ function buildPieCharts () {
             chart.draw (table, options);
         }
     });
-    /*var data = google.visualization.arrayToDataTable ([
-    ['Task', 'Hours per Day'],
-    ['Work',     11],
-    ['Eat',      2],
-    ['Commute',  2],
-    ['Watch TV', 2],
-    ['Sleep',    7]
-    ]);
-
-    var options = {
-    title: 'My Daily Activities'
-    };
-
-    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-    chart.draw(data, options);*/
 }
 
 
