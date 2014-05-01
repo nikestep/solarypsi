ec36cb90093ebccd03368b8da0313a507de5d6d1
Nik Estep <nestep@amplify.com>
2014-04-30 23:56:05 -0400

    Getting the monthly chart working for enphase sites

--- public_html/ajax/getMonthlyChartData.php
+++ public_html/ajax/getMonthlyChartData.php
@@ -2,26 +2,67 @@
 // Include the configuration file and shared methods
 include ('/home/solaryps/config/config.php');
 
+// Declare a function to return the tick display value for a month
+function getTickVal ($month) {
+    $month_str = '';
+    switch ($month) {
+        case 1:
+            $month_str = 'Jan';
+            break;
+        case 2:
+            $month_str = 'Feb';
+            break;
+        case 3:
+            $month_str = 'Mar';
+            break;
+        case 4:
+            $month_str = 'Apr';
+            break;
+        case 5:
+            $month_str = 'May';
+            break;
+        case 6:
+            $month_str = 'Jun';
+            break;
+        case 7:
+            $month_str = 'Jul';
+            break;
+        case 8:
+            $month_str = 'Aug';
+            break;
+        case 9:
+            $month_str = 'Sep';
+            break;
+        case 10:
+            $month_str = 'Oct';
+            break;
+        case 11:
+            $month_str = 'Nov';
+            break;
+        case 12:
+            $month_str = 'Dec';
+            break;
+        default:
+            break;
+    }
+    return $month_str;
+}
+
 // Open connection to MySQL database
 $db_link = new mysqli ($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE, $DB_PORT, $DB_SOCKET);
 
 // Create the object to hold the response
 $data = array ('success' => TRUE);
 
-if (isset ($_POST['siteID'])) {
-    // Figure out what 'page' of points to retrieve
-    $chart_idx = 0;
-    if (isset ($_POST['chartIdx'])) {
-        $chart_idx = intval ($_POST['chartIdx']); 
+if (isset ($_GET['siteID'])) {
+    // Get the year to retrieve
+    $year = intval (date('Y'));
+    if (isset ($_GET['year'])) {
+        $year = intval ($_GET['year']);
     }
-    $limit_start = $MONTHLY_CHART_POINT_SIZE * $chart_idx;
     
     // Retrieve the data
-    $raw_data = array ('inflow' => array (),
-                       'free_used' => array (),
-                       'free_return' => array ());
     $stmt = $db_link->prepare ("SELECT " .
-                               "    year, " .
                                "    month, " .
                                "    inflow, " .
                                "    outflow, " .
@@ -29,88 +70,71 @@ if (isset ($_POST['siteID'])) {
                                "FROM " .
                                "    data_monthly " .
                                "WHERE " .
-                               "    site_id=? " .
+                               "    site_id = ? " .
+                               "  AND " .
+                               "    year = ? " .
                                "ORDER BY " .
-                               "    year DESC, " .
-                               "    month DESC " .
-                               "LIMIT ?, ?");
-    $stmt->bind_param ('sii', $_POST['siteID'],
-                              $limit_start,
-                              $MONTHLY_CHART_POINT_SIZE);
-    $stmt->execute ();
-    $stmt->bind_result ($year, $month, $inflow, $outflow, $generation);
+                               "    month ASC");
     
-    $idx = $MONTHLY_CHART_POINT_SIZE;
-    $last_year = 0;
-    $last_month = 0;
-    while ($idx > 0 && $stmt->fetch ()) {
-        $gen_used = ($generation !== NULL && $outflow !== NULL) ? $generation - $outflow : NULL;
+    $stmt->bind_param ('si', $_GET['siteID'], $year);
+    $stmt->execute ();
+    $stmt->bind_result ($month, $inflow, $outflow, $generation);
+    
+    $idx = 1;
+    $raw_data = Array ('inflow' => Array (), 'outflow' => Array (), 'generation' => Array ());
+    while ($stmt->fetch ()) {
+        while ($idx < $month) {
+            $raw_data['inflow'][$idx] = array ('year' => $year,
+                                               'month' => $idx,
+                                               'value' => null);
+            $raw_data['outflow'][$idx] = array ('year' => $year,
+                                                'month' => $idx,
+                                                'value' => null);
+            $raw_data['generation'][$idx] = array ('year' => $year,
+                                                   'month' => $idx,
+                                                   'value' => null);
+            $idx += 1;
+        }
         
         $raw_data['inflow'][$idx] = array ('year' => $year,
                                            'month' => $month,
                                            'value' => $inflow);
-        $raw_data['free_used'][$idx] = array ('year' => $year,
-                                              'month' => $month,
-                                              'value' => $gen_used);
-        $raw_data['free_return'][$idx] = array ('year' => $year,
-                                                'month' => $month,
-                                                'value' => $outflow);
-        $idx -= 1;
-        $last_year = $year;
-        $last_month = $month;
+        $raw_data['outflow'][$idx] = array ('year' => $year,
+                                            'month' => $month,
+                                            'value' => $outflow);
+        $raw_data['generation'][$idx] = array ('year' => $year,
+                                               'month' => $month,
+                                               'value' => $generation);
+        $idx += 1;
     }
 
     $stmt->close ();
     
     // Pad end (will only execute if necessary)
-    while ($idx > 0) {
+    while ($idx <= 12) {
         // Missing data needs to be padded in
-        $last_month -= 1;
-        if ($last_month === 0) {
-            $last_month = 12;
-            $lastYear -= 1;
-        }
-        $raw_data['inflow'][$idx] = array ('year' => $last_year,
-                                           'month' => $last_month,
-                                           'value' => NULL);
-        $raw_data['free_used'][$idx] = array ('year' => $last_year,
-                                              'month' => $last_month,
-                                              'value' => NULL);
-        $raw_data['free_return'][$idx] = array ('year' => $last_year,
-                                                'month' => $last_month,
-                                                'value' => NULL);
-        $idx -= 1;
+        $raw_data['inflow'][$idx] = array ('year' => $year,
+                                           'month' => $idx,
+                                           'value' => null);
+        $raw_data['outflow'][$idx] = array ('year' => $year,
+                                            'month' => $idx,
+                                            'value' => null);
+        $raw_data['generation'][$idx] = array ('year' => $year,
+                                               'month' => $idx,
+                                               'value' => null);
+        $idx += 1;
     }
     
     // Format the data for return
-    $last_month_str = date ('F Y', strtotime ('-' . (1 + ($chart_idx * $MONTHLY_CHART_POINT_SIZE)) . ' month'));
-    $first_month_str = date ('F Y', strtotime ('-' . (($chart_idx + 1) * $MONTHLY_CHART_POINT_SIZE) . ' months'));
-    $data['title'] = "Monthly Usage from $first_month_str to $last_month_str";
-    $markings = array ();
-    array_push ($markings, array ('xaxis' => array ('from' => 0,
-                                                    'to' => 0),
-                                  'color' => '#000000'));
-    $data['options'] = array ('series' => array ('stack' => 0,
-                                                 'lines' => array ('show' => FALSE,
-                                                                    'steps' => FALSE),
-                                                 'bars' => array ('show' => TRUE,
-                                                                   'barWidth' => 0.9,
-                                                                   'align' => 'center')),
-                              'xaxis' => array ('ticks' => array (),
-                                                  'tickLength' => 0),
-                              'legend' => array ('show' => TRUE,
-                                                   'noColumns' => 3),
-                              'grid' => array ('borderWidth' => 2,
-                                                 'aboveData' => TRUE,
-                                                 'markings' => $markings));
     $data['data'] = array ();
+    $data['x_ticks'] = array ();
     $temp = array ();
     foreach ($raw_data['inflow'] as $idx => $obj) {
         // Push the tick label
         $tick = array ();
         array_push ($tick, $idx);
-        array_push ($tick, getMonthName ($obj['month'], TRUE));
-        array_push ($data['options']['xaxis']['ticks'], $tick);
+        array_push ($tick, getTickVal ($obj['month']));
+        array_push ($data['x_ticks'], $tick);
         
         // Push the data point
         $point = array ();
@@ -118,33 +142,33 @@ if (isset ($_POST['siteID'])) {
         array_push ($point, $obj['value']);
         array_push ($temp, $point);
     }
-    array_push ($data['data'], array ('label' => 'Electricity Used - Purchased',
-                                      'color' => '#' . $INFLOW_COLOR,
-                                      'data' => $temp));
+    $data['data']['inflow'] = array ('label' => 'Inflow Meter',
+                                     'color' => '#' . $INFLOW_COLOR,
+                                     'data' => $temp);
     
     $temp = array ();
-    foreach ($raw_data['free_used'] as $idx => $obj) {
+    foreach ($raw_data['outflow'] as $idx => $obj) {
         // Push the data point
         $point = array ();
         array_push ($point, $idx);
         array_push ($point, $obj['value']);
         array_push ($temp, $point);
     }
-    array_push ($data['data'], array ('label' => 'Electricity Used - Free',
-                                      'color' => '#' . $GENERATION_COLOR,
-                                      'data' => $temp));
+    $data['data']['outflow'] =  array ('label' => 'Outflow Meter',
+                                       'color' => '#' . $OUTFLOW_COLOR,
+                                       'data' => $temp);
     
     $temp = array ();
-    foreach ($raw_data['free_return'] as $idx => $obj) {
+    foreach ($raw_data['generation'] as $idx => $obj) {
         // Push the data point
         $point = array ();
         array_push ($point, $idx);
         array_push ($point, $obj['value']);
         array_push ($temp, $point);
     }
-    array_push ($data['data'], array ('label' => 'Excess Electricity - Sold',
-                                      'color' => '#' . $OUTFLOW_COLOR,
-                                      'data' => $temp));
+    $data['data']['generation'] =  array ('label' => 'Generation Meter',
+                                          'color' => '#' . $GENERATION_COLOR,
+                                          'data' => $temp);
 }
 else {
     // No site ID was provided so we cannot load data
